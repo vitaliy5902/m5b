f9cf8ef75f3f9463ea7aef26443c234ac178da86
vitaliy5902 <50337688+vitaliy5902@users.noreply.github.com>
2019-08-11 07:20:35 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Для pyany
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Для pyany по djg
 
 '''
-Для локального режима
+Для локального режима по metanit
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, "static"),
 ]
